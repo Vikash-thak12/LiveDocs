026216b520bf8b257c43e49ff987439695ffd0e9
Vikash Thakur <thakvikash622@gmail.com>
2024-08-09 22:15:40 +0530

    Updated the code

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Loader from '@/components/Loader'
+import { getClerkUser } from '@/lib/actions/user.actions'
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense'
 import { ReactNode } from 'react'
 
@@ -9,7 +10,8 @@ const Provider = ({ children }: { children: ReactNode}) => {
             <LiveblocksProvider 
             authEndpoint='/api/liveblocks-auth'
             resolveUsers={async({userIds}) => {
-                const users = await getClerkUser();
+                const users = await getClerkUser({ userIds });
+                return users;
             }}
             >
                     <ClientSideSuspense fallback={<Loader />}>
