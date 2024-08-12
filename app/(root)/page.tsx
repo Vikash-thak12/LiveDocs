ff6587753ff1e0b2c80d4e025e92a01370296efe
Vikash Thakur <thakvikash622@gmail.com>
2024-08-12 16:54:15 +0530

    Successfully showing the documents on the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,27 @@
 import AddDocumentBtn from "@/components/AddDocumentBtn"
 import Header from "@/components/Header"
+import { getDocuments } from "@/lib/actions/room.actions"
+import { dateConverter } from "@/lib/utils"
 import { SignedIn, SignIn, UserButton } from "@clerk/nextjs"
 import { currentUser } from "@clerk/nextjs/server"
 import Image from "next/image"
+import Link from "next/link"
 import { redirect } from "next/navigation"
 
 const page = async () => {
 
   const clearUser = await currentUser();
-  if(!clearUser) redirect('/sign-in')
+  if (!clearUser) redirect('/sign-in')
 
-  const documents = []
+  const roomDocuments = await getDocuments(clearUser.emailAddresses[0].emailAddress)
+  console.log("The rooms are", roomDocuments);
   
+
   return (
     <main className="home-container">
       <Header className="sticky top-0 left-0">
         <div className="flex items-center gap-2 lg:gap-5">
-          Notification 
+          Notification
           <SignedIn>
             <UserButton />
           </SignedIn>
@@ -24,21 +29,51 @@ const page = async () => {
       </Header>
 
       {
-        (documents.length > 0 ? (
-          <div></div>
+        (roomDocuments.data.length > 0 ? (
+          <div className="document-list-container">
+            <div className="document-list-title">
+              <h3 className="text-28-semibold">All Documents</h3>
+              <AddDocumentBtn
+                userId={clearUser.id}
+                email={clearUser.emailAddresses[0].emailAddress}
+              />
+            </div>
+            <ul className="document-ul">
+              {
+                roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+                  <li key={id} className="document-list-item">
+                    <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
+                      <div className="hidder rounded-md bg-dark-500 p-2 sm:block">
+                        <Image
+                          src="/assets/icons/doc.svg"
+                          alt="file"
+                          width={40}
+                          height={40}
+                        />
+                      </div>
+                      <div className="space-y-1">
+                        <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                        <p className="text-sm font-light text-blue-100">Created About {dateConverter(createdAt)}</p>
+                      </div>
+                    </Link>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
         ) : (
           <div className="document-list-empty">
             <Image
-            src="/assets/icons/doc.svg"
-            alt="document"
-            width={40}
-            height={40}
-            className="mx-auto"
-             />
-             <AddDocumentBtn
-             userId={clearUser.id}
-             email={clearUser.emailAddresses[0].emailAddress}
-              />
+              src="/assets/icons/doc.svg"
+              alt="document"
+              width={40}
+              height={40}
+              className="mx-auto"
+            />
+            <AddDocumentBtn
+              userId={clearUser.id}
+              email={clearUser.emailAddresses[0].emailAddress}
+            />
           </div>
         ))
       }
