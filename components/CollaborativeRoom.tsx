a3cff89fa280e2b1e33f8efc9babd933d3cd7a5c
Vikash Thakur <thakvikash622@gmail.com>
2024-08-13 22:30:41 +0530

    Fixing the comment functionality

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -10,9 +10,7 @@ import Image from 'next/image'
 import { updateDocument } from '@/lib/actions/room.actions'
 import Loader from './Loader'
 
-const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
-
-    const currentUserType = "editor"
+const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
 
     const [documentTitle, setDocumentTitle] = useState(roomMetadata.title)
 
@@ -120,7 +118,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => 
                             </SignedIn>
                         </div>
                     </Header>
-                    <Editor />
+                    <Editor roomId={roomId} currentUserType={currentUserType} />
                 </div>
             </ClientSideSuspense>
         </RoomProvider>
